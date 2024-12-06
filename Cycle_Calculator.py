a7a9bb0f43e84cf8124867c13e99aa7a1f58a6e9
kbartmanski <J.Bartmanski@student.tudelft.nl>
2024-12-06 18:38:22 +0100

    checks

--- Cycle_Calculator.py
+++ Cycle_Calculator.py
@@ -11,8 +11,10 @@ def cycle_calculator(bypass, m_dot_c_isa, pi_fan, pi_hpc, pi_comb, T_04, pi_tot,
     M = 0.78
     T = 220
     P = 23842
+
     T_isa = 288.15
     P_isa = 101325
+    R_air = 287
 
     # Calculate pressure ratio acroos LPC from total pressure ratio
 
@@ -22,14 +24,14 @@ def cycle_calculator(bypass, m_dot_c_isa, pi_fan, pi_hpc, pi_comb, T_04, pi_tot,
     V = M * (kappa_air * R_air * T)**0.5
 
     # Calculate core massflow from corrected core massflow
-    m_dot_c = m_dot_c_isa * P / P_isa * (T_isa / T)**0.5
+    m_dot_c = m_dot_c_isa * P / P_isa * (T_isa / T) ** 0.5
 
     # Calculate bypass massflow from bypass ratio
-    m_dot_f = bypass * m_dot_c
+    m_dot_bp = bypass * m_dot_c
 
-    # Calculate total temperature and pressure at free stream
-    T_0_a = T * (1 + (kappa_air - 1) / 2 * M**2)
-    P_0_a = P * (1 + (kappa_air - 1) / 2 * M**2)**(kappa_air / (kappa_air - 1))
+    # Calculate total temperature and pressure at free stream (a stands fro ambient)
+    T_0_a = T * (1 + (kappa_air - 1) / 2 * M ** 2)
+    P_0_a = P * (1 + (kappa_air - 1) / 2 * M ** 2) ** (kappa_air / (kappa_air - 1))
 
     # Inlet 0 -> 2
     P_0_2 = P * (1 + eta_intake * (kappa_air - 1) / 2 * M**2)**(kappa_air / (kappa_air - 1))
@@ -52,7 +54,7 @@ def cycle_calculator(bypass, m_dot_c_isa, pi_fan, pi_hpc, pi_comb, T_04, pi_tot,
     T_0_3 = T_0_25 * (1 + 1/eta_HPC * (pi_hpc**((kappa_air - 1) / kappa_air) - 1))
 
     # Combustor 3 -> 4
-    T_04 = T_04
+    # The stagnation temperature at the outlet of CC: T_04 which is given
     m_dot_fuel = (m_dot_c * cp_gas * (T_04 - T_0_3)) / (eta_combustor * LHV)
     m_dot_c2 = m_dot_c + m_dot_fuel
     p_04 = P_0_3 * pi_comb
